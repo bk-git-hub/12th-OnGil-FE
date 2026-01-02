bc38a53207854a3baa723bd90e843e6fc196b69c
Seoje1405 <hipop99@naver.com>
2026-01-02 15:44:00 +0900

    chore: auth.ts 루트 디렉터리로 이동

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import Kakao from 'next-auth/providers/kakao';
 import GoogleProvider from 'next-auth/providers/google';
-import { authConfig } from './../auth.config';
+import { authConfig } from './auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
