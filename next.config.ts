34436b9e50271399706b8b717b7c1181b36703be
Seoje1405 <hipop99@naver.com>
2026-01-05 15:35:03 +0900

    chore: 이미지 외부 도메인 허용 #7

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // 실제 이미지 호스팅 사이트
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // 더미 이미지 호스팅 사이트
+      },
+    ],
+  },
 };
 
 export default nextConfig;
