db0b2b469e6c14e0b47d65f663892f37c6e8ffce
bk-git-hub <bkprivate99@gmail.com>
2026-01-06 13:15:40 +0900

    design: 검색바 flex 특성 수정

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -9,7 +9,7 @@ export default function SearchBar() {
   const [isVoiceActive, setIsVoiceActive] = useState(false);
 
   return (
-    <div className="relative w-fit max-w-3xl">
+    <div className="relative w-fit max-w-3xl flex-1">
       {/* 음성 입력 중에도 SearchBar(부모)는 리렌더링되지 않음.
          오직 최종 결과가 나왔을 때 setQuery가 실행되면서 딱 한 번 다시 그려짐.
       */}
@@ -21,7 +21,7 @@ export default function SearchBar() {
 
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="bg-secondary-gray font-pretendard flex h-12.25 w-full items-center justify-center gap-1 rounded-lg border-2 border-[#5E5D5D] px-2.5 py-2 text-xl text-[15px] shadow-sm transition-all focus:shadow-lg focus:outline-none"
+        className="bg-secondary-gray font-pretendard flex h-12.25 w-full items-center justify-between gap-1 rounded-lg border-2 border-[#5E5D5D] px-2.5 py-2 text-xl text-[15px] shadow-sm transition-all focus:shadow-lg focus:outline-none"
       >
         <button>
           <img src="/icons/search.svg" width={16} height={16} alt="검색" />
@@ -31,7 +31,7 @@ export default function SearchBar() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="검색어 입력"
-          className=""
+          className="flex-1"
         />
 
         <div className="absolute top-1/2 right-3 flex -translate-y-1/2 items-center gap-2">
