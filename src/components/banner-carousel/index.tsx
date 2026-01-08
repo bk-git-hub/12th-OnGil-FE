c4ac1acd91d708affe86c730a2f10011d71c9fcf
bk-git-hub <bkprivate99@gmail.com>
2026-01-08 14:21:21 +0900

    feat: 광고배너 자동 넘기기 추가

--- src/components/banner-carousel/index.tsx
+++ src/components/banner-carousel/index.tsx
@@ -9,15 +9,27 @@ import {
 } from '@/components/ui/carousel';
 import { useCarouselDots } from './use-carousel-dots';
 import { DotNavigation } from './dot-navigation';
+import Autoplay from 'embla-carousel-autoplay';
 
 export function CarouselWithDots() {
   const [api, setApi] = useState<CarouselApi>();
   const { current, count, scrollTo } = useCarouselDots(api);
 
   return (
-    <div className="w-screen">
-      <Carousel setApi={setApi}>
-        <CarouselContent>
+    <div className="w-screen max-w-200">
+      <Carousel
+        setApi={setApi}
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 3000,
+            stopOnInteraction: false,
+          }),
+        ]}
+      >
+        <CarouselContent className="max-h-125 w-full">
           {Array.from({ length: 5 }).map((_, i) => (
             <CarouselItem key={i}>
               <div className="bg-card text-card-foreground flex aspect-380/275 h-full w-full items-center justify-center border-4">
