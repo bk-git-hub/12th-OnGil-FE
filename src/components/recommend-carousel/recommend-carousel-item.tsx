30c22de6b401353009391bfafe9856f20164718d
bk-git-hub <bkprivate99@gmail.com>
2026-01-12 09:35:30 +0900

    refactor: 추천 캐러셀 컴포넌트 코드 분리

---
+++ src/components/recommend-carousel/recommend-carousel-item.tsx
@@ -0,0 +1,24 @@
+import { CarouselItem } from '../ui/carousel';
+import { cn } from '@/lib/utils';
+interface RecommendCarouselItemProps {
+  children: React.ReactNode;
+  width?: number | string;
+  className?: string;
+}
+
+export function RecommendCarouselItem({
+  children,
+  width = 222,
+  className,
+}: RecommendCarouselItemProps) {
+  const flexBasis = typeof width === 'number' ? `${width}px` : width;
+
+  return (
+    <CarouselItem
+      className={cn('flex-none pl-5.5', className)}
+      style={{ flexBasis }}
+    >
+      {children}
+    </CarouselItem>
+  );
+}
