7054ceacf6f25820a300aca79d4d393db9166e5d
bk-git-hub <bkprivate99@gmail.com>
2026-01-13 13:30:21 +0900

    chore: 상품 api 관련 타입정의

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,14 +1,66 @@
 export interface Product {
   id: number;
-  brand: string;
-  productName: string;
-  price: number;
-  isOnSale: boolean;
+  name: string;
   originalPrice?: number;
-  imageUrl: string;
+  discountRate?: number;
+  discountPrice?: number;
+  finalPrice: number;
+  thumbnailImageUrl: string;
+  brandName: string;
+  productType: 'NORMAL' | 'AUCTION' | string; // literal type if known, else string
+  productTypeDescription: string;
+  viewCount: number;
+  purchaseCount: number;
+  reviewCount: number;
+}
+
+export interface SortInfo {
+  direction: string;
+  nullHandling: string;
 }
 
 export interface SpecialPriceCardProps extends Product {
   reviewCount: number;
   saleRate: number;
+}
+
+export interface ProductListReturnType {
+  status: string;
+  timestamp: string;
+}
+export interface ProductListDataType {
+  totalPages: number;
+  totalElements: number;
+  ascending: boolean;
+  property: string;
+  ignoreCase: boolean;
+}
+
+export interface PageableInfo {
+  paged: boolean;
+  pageNumber: number;
+  pageSize: number;
+  offset: number;
+  sort: SortInfo[];
+  unpaged: boolean;
+}
+
+export interface PaginatedData {
+  totalPages: number;
+  totalElements: number;
+  pageable: PageableInfo;
+  first: boolean;
+  last: boolean;
+  size: number;
+  content: Product[];
+  number: number;
+  sort: SortInfo[];
+  numberOfElements: number;
+  empty: boolean;
+}
+
+export interface ProductListResponse {
+  status: string;
+  timestamp: string;
+  data: PaginatedData;
 }
