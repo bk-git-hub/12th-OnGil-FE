c04eb8ead608869acb0838ba102d760cdb343060
Seoje1405 <hipop99@naver.com>
2026-01-15 19:41:56 +0900

    refactor: 필요없는 파일 제거 #25

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,4 +1,3 @@
 export { ProductList } from './product-list';
-export { ProductDetail } from './product-detail';
 export { ProductCard } from './product-card';
 export { ProductGrid } from './product-grid';
