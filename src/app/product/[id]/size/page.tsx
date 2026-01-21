2025d189a130ba99c9c30dac6d6cd9e027406ea2
Seoje1405 <hipop99@naver.com>
2026-01-21 12:20:07 +0900

    feat: size 탭 페이지 구현 #31

--- src/app/product/[id]/size/page.tsx
+++ src/app/product/[id]/size/page.tsx
@@ -1,8 +1,36 @@
-export default function ProductSizePage() {
+import SizeInfo from '@/components/product/size/size-info';
+import SizeGuideSection from '@/components/product/size/size-guide-section';
+import { fetchUserBodyInfo, fetchSizeAnalysis } from '@/mocks/size';
+
+interface ProductSizePageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function ProductSizePage({
+  params,
+}: ProductSizePageProps) {
+  const { id: productId } = await params;
+  const productType = 'top';
+  const userInfo = await fetchUserBodyInfo();
+  const analysisData = userInfo
+    ? await fetchSizeAnalysis(productId, userInfo.height, userInfo.weight)
+    : null;
+
   return (
-    <div className="rounded-lg bg-gray-50 py-20 text-center text-gray-500">
-      <p>사이즈 영역입니다.</p>
-      <p className="mt-2 text-sm">이곳에 사이즈 컴포넌트가 들어갑니다.</p>
+    <div className="font-pretendard flex flex-col gap-10 px-4 py-8 pb-20">
+      {/* 상단 실측 정보 */}
+      <SizeInfo />
+
+      <hr className="border-gray-100" />
+
+      {/* 사이즈 가이드 섹션 */}
+      <section className="space-y-4">
+        <SizeGuideSection
+          productType={productType}
+          userInfo={userInfo}
+          analysisData={analysisData}
+        />
+      </section>
     </div>
   );
 }
