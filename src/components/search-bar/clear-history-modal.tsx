b397616c46de10b7b93e0013a4e5ba9045246996
bk-git-hub <bkprivate99@gmail.com>
2026-01-22 10:48:01 +0900

    design: 전체 삭제 모달창 스타일 적용 #30

--- src/components/search-bar/clear-history-modal.tsx
+++ src/components/search-bar/clear-history-modal.tsx
@@ -8,35 +8,33 @@ export default function ClearHistoryModal({
   onClear,
 }: ClearHistoryModalProps) {
   return (
-    <div className="animate-in fade-in zoom-in relative z-101 w-full max-w-[320px] transform rounded-2xl bg-white p-6 shadow-2xl transition-all duration-200">
-      <div className="flex flex-col items-center text-center">
-        <p className="mb-6 text-sm leading-relaxed font-normal text-gray-500">
-          모든 최근 검색 기록이 삭제됩니다.
-          <br />
-          정말 진행하시겠습니까?
-        </p>
+    <div className="font-pretendard animate-in fade-in zoom-in relative z-101 flex min-h-75 w-full min-w-85.5 transform flex-col rounded-2xl bg-white p-6 shadow-2xl transition-all duration-200">
+      <p className="flex h-full flex-1 items-center justify-center text-center text-2xl leading-relaxed font-medium text-black">
+        최근 검색어를
+        <br />
+        모두 삭제할까요?
+      </p>
 
-        <div className="flex w-full gap-3">
-          <button
-            type="button"
-            onPointerDown={(e) => e.preventDefault()}
-            onClick={onClose}
-            className="flex-1 rounded-xl bg-gray-100 px-4 py-3 text-sm font-semibold text-gray-700 transition-colors hover:bg-gray-200"
-          >
-            취소
-          </button>
-          <button
-            type="button"
-            onPointerDown={(e) => e.preventDefault()}
-            onClick={() => {
-              onClear();
-              onClose();
-            }}
-            className="flex-1 rounded-xl bg-red-600 px-4 py-3 text-sm font-semibold text-white shadow-lg shadow-red-200 transition-colors hover:bg-red-700"
-          >
-            삭제
-          </button>
-        </div>
+      <div className="flex w-full gap-3 text-xl font-bold">
+        <button
+          type="button"
+          onPointerDown={(e) => e.preventDefault()}
+          onClick={onClose}
+          className="flex-1 rounded-3xl bg-[#d1d1d1] px-4 py-4.5 text-black"
+        >
+          취소
+        </button>
+        <button
+          type="button"
+          onPointerDown={(e) => e.preventDefault()}
+          onClick={() => {
+            onClear();
+            onClose();
+          }}
+          className="bg-ongil-teal flex-1 rounded-3xl px-4 py-4.5 text-white"
+        >
+          삭제
+        </button>
       </div>
     </div>
   );
