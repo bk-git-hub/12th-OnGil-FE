9c7236d1421bc2b9bec3215b8c9be6162547f579
Seoje1405 <hipop99@naver.com>
2026-01-25 18:15:00 +0900

    refactor: 페이지 이동을 state로 변경 #33

--- src/components/product/product-tab.tsx
+++ src/components/product/product-tab.tsx
@@ -1,48 +1,41 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { useProductInteraction } from '@/components/product';
 import { PRODUCT_TABS } from '@/config/product-tabs';
+import { Button } from '../ui/button';
 
 // 상품 상세 정보 탭 컴포넌트
 
 interface ProductTabProps {
-  productId: string;
+  activateTab: string;
+  onTabChange: (tabId: string) => void;
 }
 
-export function ProductTab({ productId }: ProductTabProps) {
-  const pathname = usePathname();
+export function ProductTab({ activateTab, onTabChange }: ProductTabProps) {
   const { triggerScroll } = useProductInteraction();
-
+  const handleTabClick = (tabId: string) => {
+    triggerScroll();
+    onTabChange(tabId);
+  };
   return (
     <div className="font-pretendard flex h-12 w-full bg-[#D9D9D9]">
       {PRODUCT_TABS.map((tab) => {
-        const href = `/product/${productId}/${tab.id}`;
-
-        // /product/id일 경우 desc 탭 활성화.
-        const isActive =
-          pathname.endsWith(`/${tab.id}`) ||
-          (tab.id === 'desc' && pathname === `/product/${productId}`);
-
         return (
-          <Link
+          <button
             key={tab.id}
-            href={href}
-            scroll={false} // 기본 스크롤 방지
-            onClick={() => triggerScroll()}
+            onClick={() => handleTabClick(tab.id)}
             className={cn(
               'relative flex-1 py-3 text-center text-sm transition-colors outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-inset',
               // 선택됨: 흰색 배경으로 아래 콘텐츠와 자연스럽게 연결
               // 선택안됨: 회색 배경
-              isActive
+              activateTab === tab.id
                 ? 'bg-white font-bold text-black'
                 : 'font-medium text-[#8B8A8A] hover:text-gray-600',
             )}
           >
             {tab.label}
-          </Link>
+          </button>
         );
       })}
     </div>
