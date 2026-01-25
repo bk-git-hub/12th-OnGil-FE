9b7a329bf22bdd82ca77b2a34f80d776ebfd28f3
Seoje1405 <hipop99@naver.com>
2026-01-25 18:12:52 +0900

    remove: 상품 상세 탭 페이지 파일 삭제 #33

--- src/app/product/[id]/review/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProductReviewPage() {
-  return (
-    <div className="rounded-lg bg-gray-50 py-20 text-center text-gray-500">
-      <p>리뷰 영역입니다.</p>
-      <p className="mt-2 text-sm">이곳에 리뷰 리스트 컴포넌트가 들어갑니다.</p>
-    </div>
-  );
-}
