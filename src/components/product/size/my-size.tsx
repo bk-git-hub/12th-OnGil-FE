3d8bdb8527ace2884fd906e4eb50043827d0a293
Seoje1405 <hipop99@naver.com>
2026-01-31 16:01:59 +0900

    design: 텍스트 및 레이아웃 정렬 개선 #45

--- src/components/product/size/my-size.tsx
+++ src/components/product/size/my-size.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { Ruler, Edit2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
 import { UserBodyInfo } from '@/mocks/size';
 import Link from 'next/link';
 
@@ -30,30 +28,20 @@ export function MySize({ userInfo, productType, onEdit }: MySizeProps) {
   };
 
   return (
-    <div className="mt-8 flex flex-col gap-12">
-      <h3 className="text-center text-xl font-bold text-gray-900">
-        내 사이즈 정보
-      </h3>
-      <Card className="border border-gray-100 bg-white shadow-sm">
-        <CardContent className="flex items-center justify-between px-6 py-5">
-          <div className="flex items-center gap-12">
-            <div className="bg-ongil-mint flex h-10 w-10 shrink-0 items-center justify-center rounded-full">
-              <Ruler className="h-5 w-5" />
-            </div>
-            <div className="text-xl font-bold text-gray-900">
-              {userInfo.height}cm / {userInfo.weight}kg / {getUserSizeText()}
-            </div>
-          </div>
-        </CardContent>
-      </Card>
+    <div className="mt-8 flex flex-col items-center gap-12 text-center text-2xl leading-6 font-semibold not-italic">
+      <span>내 사이즈 정보</span>
+      <span>
+        {userInfo.height}cm / {userInfo.weight}kg / {getUserSizeText()}
+      </span>
 
       <Button
-        variant="outline"
-        className="bg-ongil-teal w-full rounded-xl border-gray-300 py-7 text-base font-bold text-white hover:bg-[#00252a] hover:text-white"
-        asChild
+        variant="ghost"
+        className="bg-ongil-teal h-[55px] w-[239px] rounded-xl px-5 py-4 text-white"
       >
         <Link href="/body-info" scroll={false}>
-          <Edit2 className="mr-2 h-5 w-5" />내 정보 수정하기
+          <span className="text-center text-xl leading-normal font-semibold not-italic">
+            내 정보 수정하기
+          </span>
         </Link>
       </Button>
     </div>
