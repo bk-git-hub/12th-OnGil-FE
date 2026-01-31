e42c99f3c9059b8582bea1e54a497716d0cda9ea
Seoje1405 <hipop99@naver.com>
2026-01-31 13:02:59 +0900

    design: 상품 정보 글짜 크기, 여백 수정 #45

--- src/components/product/product-info.tsx
+++ src/components/product/product-info.tsx
@@ -3,7 +3,7 @@
 import { type Ref } from 'react';
 import { Product } from '@/types/domain/product';
 import { Button } from '@/components/ui/button';
-import { Bell } from 'lucide-react';
+import { StarRating } from '../ui/star-rating';
 
 interface ProductInfoProps {
   product: Product;
@@ -20,60 +20,55 @@ interface ProductInfoProps {
 export default function ProductInfo({ product, discountRef }: ProductInfoProps) {
   const hasDiscount = !!product.discountRate;
   return (
-    <div className="font-pretendard px-4 py-6">
+    <div className="font-pretendard p-[22px] leading-[18px] not-italic">
       {/* 8.1.1 (1) 브랜드 */}
-      <div className="mb-1 text-xl font-bold text-gray-500">
-        {product.brandName}
-      </div>
+      <div className="mb-6 text-xl font-extrabold">{product.brandName}</div>
       {/* 8.1.1 (3) 상품명 */}
-      <h1 className="line-clamp-2 text-2xl leading-tight font-bold break-keep text-gray-900">
+      <h1 className="mb-6 px-2 text-2xl leading-tight font-medium">
         {product.name}
       </h1>
-      {/* 가격 정보 */}
-      <div className="mt-4">
-        {hasDiscount ? (
-          <div className="flex flex-col">
-            <div className="flex items-end gap-2">
-              {/* 할인가 (현재 판매가) */}
-              <span className="text-4xl font-bold text-black">
-                <span className="sr-only">할인 판매가</span>
-                {product.finalPrice.toLocaleString()}
-                <span className="text-2xl font-normal">원</span>
-              </span>
-
-              {/* 할인율 */}
-              <span className="mb-1 text-xl font-bold text-red-600">
-                <span className="sr-only">할인율</span>
-                {product.discountRate}%
-              </span>
-            </div>
-
+      {hasDiscount ? (
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-3">
+            {/* 할인가 (현재 판매가) */}
+            <span className="text-4xl font-bold">
+              <span className="sr-only">할인 판매가</span>
+              <span>{product.finalPrice.toLocaleString()}원</span>
+            </span>
             {/* 원가 (취소선)*/}
-            <del className="mt-1 text-xl text-gray-400">
+            <del className="text-xl text-[#0000004F]">
               <span className="sr-only">정상가</span>
               {product.price.toLocaleString()}원
             </del>
           </div>
-        ) : (
-          <span className="text-2xl font-bold text-black">
-            <span className="sr-only">판매가</span>
-            {product.price.toLocaleString()}원
+          <span className="mb-1 text-xl font-bold text-red-600">
+            <span className="sr-only">할인율</span>
+            {product.discountRate}%
           </span>
-        )}
+        </div>
+      ) : (
+        <span className="text-4xl font-bold">
+          <span className="sr-only">판매가</span>
+          {product.price.toLocaleString()}원
+        </span>
+      )}
+      <div className="mt-2 flex items-center gap-2">
+        <StarRating rating={product.reviewRating} size={24} />
+        <span className="font-Poppins text-sm leading-5 font-semibold not-italic">
+          ({product.reviewCount})
+        </span>
       </div>
-      {/* 리뷰 별점 정보 추가해야함. */}
-      <h1 className="mt-3 text-gray-600">리뷰 별점 ★★★★☆</h1>
-
       {/* 8.2 할인 알림 받기 버튼, 추후 알림 로직 구현 해야함. */}
-      <Button
-        ref={discountRef}
-        variant="outline"
-        className="mt-6 w-full rounded-xl border-gray-300 bg-[#00363D] py-7 text-base font-bold text-white hover:bg-[#00252a] hover:text-white"
-        onClick={() => alert('할인가 알림 신청 완료!')}
-      >
-        <Bell className="mr-2 h-5 w-5" />
-        할인 알림 받기
-      </Button>
+      <div className="flex justify-center">
+        <Button
+          ref={discountRef}
+          variant="outline"
+          className="bg-ongil-teal mt-6 h-[66px] w-[232px] justify-center rounded-3xl py-5 text-center text-xl leading-normal font-bold text-white"
+          onClick={() => alert('할인가 알림 신청 완료!')}
+        >
+          할인 알림
+        </Button>
+      </div>
     </div>
   );
 }
