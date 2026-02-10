061e5697d4788949203dc46fbaea15a81facd7f4
bk-git-hub <bkprivate99@gmail.com>
2026-02-10 09:46:56 +0900

    feat: 추천 카테고리 캐러셀 api 연동

---
+++ src/components/recommend-carousel/recommend-category-container.tsx
@@ -0,0 +1,24 @@
+import { api } from '@/lib/api-client';
+import { Category } from '@/types/domain/category';
+import { RecommendCarousel } from './recommend-carousel';
+import { RecommendCarouselItem } from './recommend-carousel-item';
+import { RecommendedCategoryCard } from './recommended-category-card';
+
+export default async function RecommendCategoryContainer() {
+  const categories = await api.get<Category[]>('/categories/recommended-sub');
+
+  console.log(categories);
+
+  return (
+    <div>
+      <h2>추천 카테고리</h2>
+      <RecommendCarousel heading="추천 카테고리">
+        {categories.map((category) => (
+          <RecommendCarouselItem key={category.categoryId}>
+            <RecommendedCategoryCard category={category} />
+          </RecommendCarouselItem>
+        ))}
+      </RecommendCarousel>
+    </div>
+  );
+}
