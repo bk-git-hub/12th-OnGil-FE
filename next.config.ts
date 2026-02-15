ad440300cb7d063f1e02d74cbb9dcd64ace9e12b
Seoje1405 <hipop99@naver.com>
2026-02-15 13:46:52 +0900

    chore: 구글 프로필 이미지 호스팅 설정

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+
+      {
+        protocol: 'https',
         hostname: 'image.msscdn.net',
       },
       {
