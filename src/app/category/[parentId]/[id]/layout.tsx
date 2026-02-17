1ba2091f8ca029b532cbe384e9416be538090041
Seoje1405 <hipop99@naver.com>
2026-02-18 00:02:18 +0900

    refactor: 카테고리 parent/product 라우트 레이아웃 분리 및 초기 탭 동기화

---
+++ src/app/category/[parentId]/[id]/layout.tsx
@@ -0,0 +1,23 @@
+import SubCategorySliderContainer from '@/components/sub-category-slider/sub-category-slider-container';
+import CategoryParentHeaderBar from '@/components/category/category-parent-header-bar';
+import { Suspense } from 'react';
+
+export default async function CategoryProductLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ parentId: string }>;
+}) {
+  return (
+    <section className="flex h-full min-h-0 flex-col">
+      <CategoryParentHeaderBar />
+
+      <Suspense>
+        <SubCategorySliderContainer params={params} />
+      </Suspense>
+
+      <main className="min-h-0 flex-1">{children}</main>
+    </section>
+  );
+}
